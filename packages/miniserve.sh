25a8e4e3c774cbb21f0cbe7d0f7656f878eb8510
Hero <vndhero@gmail.com>
2025-04-11 22:29:55 +0700

    fix: add some packages

---
+++ packages/miniserve.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo curl -Lo /usr/local/bin/miniserve https://github.com/svenstaro/miniserve/releases/download/v0.28.0/miniserve-0.28.0-x86_64-unknown-linux-gnu
+sudo chmod +x /usr/local/bin/miniserve
